6392a12e7da1394c6331d4a1167fa0deba4bf5f3
yueyuem77 <ym2980@columbia.edu>
2025-06-23 13:41:12 -0400

    Fix: add missing os import

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import streamlit as st
 from PIL import Image
 
+import os
 
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
